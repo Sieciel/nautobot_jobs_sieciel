7fc815dc18136d78f9f8bbd577ae865ab59d12b2
Sieciel <104871657+Sieciel@users.noreply.github.com>
2024-03-28 12:08:37 +0100

    com2

--- jobs/my_job.py
+++ jobs/my_job.py
@@ -5,7 +5,7 @@ class SimpleLogJob(Job):
         name: "Simle Log Message"
         description = "Logs a simple message to the job output."
 
-    # message = StringVar (description="This is me doing code")
+    message = StringVar (description="This is me doing code")
 
     # def run(self, **kwargs):
     #     self.logger.info(str(kwargs))
